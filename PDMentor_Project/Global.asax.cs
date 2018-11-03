7540f8f5d9c181f14051e55b4a440e3d38a2b694
aaronho1 <aaronho1@asu.edu>
2018-11-03 09:46:05 -0700

    deleted a bunch of stuff to make it look like sparkychat. It worked in the PDMentor2018Copy in Azure. It should work here

--- PDMentor_Project/Global.asax.cs
+++ PDMentor_Project/Global.asax.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Optimization;
-using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 
@@ -14,8 +12,8 @@ namespace PDMentor_Project
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
